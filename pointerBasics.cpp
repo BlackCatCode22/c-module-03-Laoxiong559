1470cafec5f22e391fb9103d940edea708a9f2e4
Laoxiong559 <0481711@my.scccd.edu>
2025-02-13 18:10:08 -0800

    pointerBasics.cpp

--- pointerBasics.cpp
+++ pointerBasics.cpp
@@ -1,6 +1,4 @@
-// pointerBasics.cpp
-// sample code for Module 03
-// written by dH 2/13/25, Fresno, CA
+
 
 #include <iostream>
 using namespace std;
@@ -18,10 +16,8 @@ int main() {
     cout << "\n The address of pointer myIntPointer is: " << myIntPointer << endl;
     cout << "\n The value of the variable named num1 using the pointer named myIntPointer is: " << *myIntPointer << "\n";
 
-    // Dereference the int pointer and give it a new value.
     *myIntPointer = 57;
 
-    // Output the new value of num1.
     cout << "\n The value of num1 is: " << num1 << "\n";
 
     return 0;
